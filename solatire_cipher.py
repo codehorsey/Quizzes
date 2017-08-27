c664b4cd5edc3de8e6a84d3392258551b67ced82
codehorsey <codehorsey@gmail.com>
2017-08-27 05:11:09 -0500

    Finished encypt phase

--- solatire_cipher.py
+++ solatire_cipher.py
@@ -1,5 +1,3 @@
-
-
 def return_alpha(s):
 	ns = str()
 	for letter in s:
@@ -29,30 +27,57 @@ def convert_str_to_num_using_key(s, key):
 	for letter in s:
 		if letter.isalpha():
 			nums.append(key[letter])
-			
-
 	return nums
 
-def app(s, key):
+def convert_num_to_str_using_key(nums, key):
+	s = str()
+	for n in nums:
+		if n.isdigit():
+			print key[n]
+			s += key[n]
+
+
+def merge_lists(s, s2):
+	mylist = list()
+	for i, j in zip(s, s2):
+		temp = i + j
+		if temp > 26:
+			temp = temp-26
+		mylist.append(temp)
+
+	return mylist
+	
+def return_numbers_into_str(nums):
+	for n in nums:
+		lookup_key_with_value(n, abckey)
+
+
+def lookup_key_with_value(n, mydict):
+	for key in mydict:
+		if mydict[key] == n:
+			return key
+def convert_merged_lists_into_one_string(merged, abckey):
+	s = str()
+	for n in merged:
+		s += lookup_key_with_value(n, abckey)
+	return s
+
+def str_to_nums(s, key):
 	s = s.upper()	 
 	s = return_alpha(s)
 	s = split_into_4(s)
 	s = convert_str_to_num_using_key(s, key)
 	return s
 
-
 abc = 'abcdefghijklmnopqrstuvwxyz'.upper()
 abckey = string_into_numerical_dict(abc)
 
 s = 'Code in Ruby, live longer!'
-s = app(s, abckey)
+s = str_to_nums(s, abckey)
 
 keystream = 'DWJXH YRFDG TMSHP UURXJ'
-s2 =  app(keystream, abckey)
+s2 =  str_to_nums(keystream, abckey)
 
-
-for i, j in zip(s, s2):
-	temp = i + j
-	if temp > 26:
-		temp = temp-26
-	print temp
+merged = merge_lists(s,s2)
+s = convert_merged_lists_into_one_string(merged, abckey)
+print split_into_4(s)
