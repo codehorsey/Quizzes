fdb52dfd89c3a57352008676335b84b8b542835e
codehorsey <codehorsey@gmail.com>
2017-08-27 23:19:14 -0500

    Added docstrings, refactor

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # Quizzes
+The Solitaire Cipher (#1)
+
+Cryptologist Bruce Schneier designed the hand cipher "Solitaire" for Neal Stephenson's book "Cryptonomicon". Created to be the first truly secure hand cipher, Solitaire requires only a deck of cards for the encryption and decryption of messages.
+
+While it's true that Solitaire is easily completed by hand given ample time, using a computer is much quicker and easier. Because of that, Solitaire conversion routines are available in many languages, though I've not yet run across one in Ruby.
+
+This week's quiz is to write a Ruby script that does the encryption and decryption of messages using the Solitaire cipher.
+
+Let's look at the steps of encrypting a message with Solitaire.
+
+1. Discard any non A to Z characters, and uppercase all remaining letters. Split the message into five character groups, using Xs to pad the last group, if needed. If we begin with the message "Code in Ruby, live longer!", for example, we would now have:
+
+CODEI NRUBY LIVEL ONGER
+
+2. Use Solitaire to generate a keystream letter for each letter in the message. This step is detailed below, but for the sake of example let's just say we get:
+
+DWJXH YRFDG TMSHP UURXJ
+
+3. Convert the message from step 1 into numbers, A = 1, B = 2, etc:
+
+3 15 4 5 9  14 18 21 2 25  12 9 22 5 12  15 14 7 5 18
+
+4. Convert the keystream letters from step 2 using the same method:
+
+4 23 10 24 8  25 18 6 4 7  20 13 19 8 16  21 21 18 24 10
+
+5. Add the message numbers from step 3 to the keystream numbers from step 4 and subtract 26 from the result if it is greater than 26. For example, 6 + 10 = 16 as expected, but 26 + 1 = 1 (27 - 26):
+
+7 12 14 3 17  13 10 1 6 6  6 22 15 13 2  10 9 25 3 2
+
+6. Convert the numbers from step 5 back to letters:
+
+GLNCQ MJAFF FVOMB JIYCB
+
+That took a while to break down, but it's really a very simple process. Decrypting with Solitaire is even easier, so let's look at those steps now. We'll work backwards with our example now, decrypting "GLNCQ MJAFF FVOMB JIYCB".
--- solatire_cipher.py
+++ solatire_cipher.py
@@ -1,11 +1,27 @@
-def return_alpha(s):
+def str_to_alpha(s):
+	"""Cleans a string to have only alphanumeric chars.
+	
+	Args:
+	    s (str): string to be cleaned
+	
+	Returns:
+	    str: cleaned string containing only alphanumeric
+	"""
 	ns = str()
 	for letter in s:
 		if letter.isalpha():
 			ns += letter
 	return ns
 
-def split_into_4(s):
+def str_to_four(s):
+	"""Splits string into 4 groups of 5 chars.
+	
+	Args:
+	    s (str): String to be split.
+	
+	Returns:
+	    str: String split into 4 groups of 5 chars separated by ' '.
+	"""
 	ns = str()
 	for count, letter in enumerate(s,1):
 		ns += letter
@@ -14,7 +30,15 @@ def split_into_4(s):
 			ns += ' '
 	return ns
 
-def string_into_numerical_dict(s):
+def str_to_numbered_dict(s):
+	"""Pair each char of a string to a number which increments starting at 1.
+	
+	Args:
+	    s (str): Pair str with numbers.
+	
+	Returns:
+	    dict: Dictionary with each char of param paired with numbers.
+	"""
 	ndict = dict()
 
 	for count, letter in enumerate(s,1):
@@ -22,62 +46,97 @@ def string_into_numerical_dict(s):
 
 	return ndict
 
-def convert_str_to_num_using_key(s, key):
+def str_to_num_using_key(s, key):
+	"""Turn string into a series of numbers based on provided key dictionary.
+	
+	Args:
+	    s (str): String to be converted into numbers.
+	    key (dict): Dictionary that pairs each alpha to a number. 
+	
+	Returns:
+	    list: List of numbers from converted string.
+	"""
 	nums = list()
 	for letter in s:
 		if letter.isalpha():
 			nums.append(key[letter])
 	return nums
 
-def convert_num_to_str_using_key(nums, key):
-	s = str()
-	for n in nums:
-		if n.isdigit():
-			print key[n]
-			s += key[n]
-
-
-def merge_lists(s, s2):
+def merge_lists(l1, l2):
+	"""
+	Merges each index of param1 and param2 to create a new list. If merged indexes are greater than 26, subtract 26.
+	
+	Args:
+	    l1 (list): list of numbers.
+	    l2 (list): list of numbers.
+	
+	Returns:
+	    list: List of combined indexes of l1 & l2.
+	"""
 	mylist = list()
-	for i, j in zip(s, s2):
+	for i, j in zip(l1, l2):
 		temp = i + j
 		if temp > 26:
 			temp = temp-26
 		mylist.append(temp)
 
 	return mylist
+
+def value_to_key(v, mydict):
+	"""Iterate through dictionary until matched value is matched.
 	
-def return_numbers_into_str(nums):
-	for n in nums:
-		lookup_key_with_value(n, abckey)
-
-
-def lookup_key_with_value(n, mydict):
+	Args:
+	    v (int): Value to use.
+	    mydict (dict): Dictionary provided.
+	
+	Returns:
+	    key: Return key of provided value.
+	"""
 	for key in mydict:
-		if mydict[key] == n:
+		if mydict[key] == v:
 			return key
-def convert_merged_lists_into_one_string(merged, abckey):
+
+def num_list_to_string(nums, key):
+	"""Convert numbers into string using provided key.
+	
+	Args:
+	    nums (list): List of numbers.
+	    key (dict): Dictionary pairing alphabet to numbers.
+	
+	Returns:
+	    str: A alphanumeric string converted using provided key.
+	"""
 	s = str()
-	for n in merged:
-		s += lookup_key_with_value(n, abckey)
+	for n in nums:
+		s += value_to_key(n, key)
 	return s
 
 def str_to_nums(s, key):
+	"""
+	Take a string and convert to uppercase, remve non-alphanumeric characters, split into 4 groups of 5 chars, and convert into numbers using provided dictionary key.
+	
+	Args:
+	    s (str): Description
+	    key (dict): Dictionary provided to convert alpha -> numeric using key, values
+	
+	Returns:
+	    list: A list of nums converted from string.
+	"""
 	s = s.upper()	 
-	s = return_alpha(s)
-	s = split_into_4(s)
-	s = convert_str_to_num_using_key(s, key)
+	s = str_to_alpha(s)
+	s = str_to_four(s) # I don't think I need this until end.
+	s = str_to_num_using_key(s, key)
 	return s
 
 abc = 'abcdefghijklmnopqrstuvwxyz'.upper()
-abckey = string_into_numerical_dict(abc)
+abckey = str_to_numbered_dict(abc)
 
-s = 'Code in Ruby, live longer!'
-s = str_to_nums(s, abckey)
+s1 = 'Code in Ruby, live longer!'
+s1 = str_to_nums(s1, abckey)
 
 keystream = 'DWJXH YRFDG TMSHP UURXJ'
-s2 =  str_to_nums(keystream, abckey)
+s2 = str_to_nums(keystream, abckey)
 
-merged = merge_lists(s,s2)
-s = convert_merged_lists_into_one_string(merged, abckey)
-print split_into_4(s)
+merged = merge_lists(s1,s2)
+s = num_list_to_string(merged, abckey)
+print str_to_four(s)
